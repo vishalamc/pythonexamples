7e6657994e78560302adae59e3c83cef53ca53bd
vishalamc <vishalamc@gmail.com>
2019-01-21 09:33:16 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,6 @@
+# this program shows addition of two numbers
+
+num1=float(input("Enter num1\n"))
+num2=float(input("Enter num2\n"))
+num3=num1+num2
+print("Addition of",num1,"and ",num2,"is:", num3)
