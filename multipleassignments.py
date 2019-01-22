3b86b3b7e3b8c73cb30c4be950397ee19270901c
vishalamc <vishalamc@gmail.com>
2019-01-22 09:51:41 +0530

    Create multipleassignments.py

---
+++ multipleassignments.py
@@ -0,0 +1,13 @@
+#This Program shows multiple assignments 
+# swapping the value 
+p=10
+q=20
+print("before swap")
+print("p=",p)
+print("q=",q)
+print("after swap")
+p,q=q,p #this shows multiple assignments
+
+print("p=",p)
+print("q=",q)
+
